6579cbf0d8b6c0bd731413597e5d050928d8c9d2
nejad <nejad.se@gmail.com>
2023-08-18 20:45:13 +0200

    Update deleteMeAnytime.php
    Edit in GitHub

--- Bro Code PHP/deleteMeAnytime.php
+++ Bro Code PHP/deleteMeAnytime.php
@@ -1,4 +1,5 @@
 <?php
 $numSet = array(4,6,8,10,37);
 echo "Maximum Number is: ". max($numSet). "<br>";
-echo "Minimum Number is: ". min($numSet);
+echo "Minimum Number is: ". min($numSet). "<br>";
+
